70c49b6185e8c6612ec4664412438a6aec345bb3
dustcloud777 <38367311+dustcloud777@users.noreply.github.com>
2021-05-27 22:16:35 -0400

    reverse trollage

--- CivBot.py
+++ CivBot.py
@@ -65,7 +65,7 @@ async def on_message(ctx):
                 if 'linux' in lower_content and not 'gnu' in lower_content and 60 > time.time() - last_times.get('gnu_linux', 0):
                     last_times['gnu_linux'] = time.time()
                     await ctx.channel.send(gnu_linux)
-                if ctx.author.id == 318296849775722497 and  ctx.guild.id == 742831212711772261:
+                if ctx.author.id == 318296849775722497 and  ctx.guild.id == 742831212711772261 and do_orange_response:
                     await ctx.channel.send("blocked. That response was too stupid to continue any discussion with orange wizard")
                     do_orange_response = False
                 message = ""
